fe35da70220a811001573fe760865c7842af23b2
delevinying <delevinyyc@gmail.com>
2019-02-01 13:28:16 +0800

    扩展1生成两个三角形
    扩展1生成两个三角形

---
+++ testOpenGL/testOpenGL/part2_1.cpp
@@ -0,0 +1,140 @@
+//
+//  part2_1.cpp
+//  testOpenGL
+//
+//  Created by 应云超 on 2019/2/1.
+//  Copyright © 2019年 应云超. All rights reserved.
+//
+
+//扩展1:添加更多顶点到数据中，使用glDrawArrays，尝试绘制两个彼此相连的三角形
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window,int width,int height);
+void processInput(GLFWwindow *window);
+const unsigned int SCR_WIDTH = 700;
+const unsigned int SCR_HEIGHT = 600;
+//上面的内容和part1一样
+
+const char *vertexShaderSoucre = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"void main()\n"
+"{\n"
+"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\0";
+const char *fragmentShaderSource = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n\0";
+
+
+int main(){
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    //接着创建窗口
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "2.画两个相连的三角形", NULL, NULL);
+    if(window == NULL){
+        std::cout << "窗口创建失败" << std::endl;
+        glfwTerminate();//终止
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//监听窗口改变
+    //glad 的问题
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    
+    //下面是绘制三角形的主要代码
+    int vertexShader = glCreateShader(GL_VERTEX_SHADER);//创建顶点着色器
+    glShaderSource(vertexShader,1,&vertexShaderSoucre,NULL);
+    glCompileShader(vertexShader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader,GL_COMPILE_STATUS,&success);
+    if(!success){
+        glGetShaderInfoLog(vertexShader,512,NULL,infoLog);//获取出错log
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    
+    int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);//片段着色器
+    glShaderSource(fragmentShader,1,&fragmentShaderSource,NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader,GL_COMPILE_STATUS,&success);
+    if(!success){
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);//获取出错log
+        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+    }
+    //把两个着色器关联上一个着色器program
+    int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram,vertexShader);//顶点着色器和片段着色器都关联上着色器程序
+    glAttachShader(shaderProgram,fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram,GL_LINK_STATUS,&success);
+    if(!success){
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);//出错log
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    }
+    glDeleteShader(vertexShader);//删除，顶点着色器和片段着色器都不需要再使用，数据都在着色器程序内
+    glDeleteShader(fragmentShader);
+    
+    //三角形的三个顶点坐标
+    float vertices[] = {
+        -0.9f, -0.5f, 0.0f,  // left
+        -0.0f, -0.5f, 0.0f,  // right
+        -0.45f, 0.5f, 0.0f,  // top
+        // 另一个三角形
+        0.0f, -0.5f, 0.0f,  // left
+        0.9f, -0.5f, 0.0f,  // right
+        0.45f, 0.5f, 0.0f   // top
+    };
+    
+    //顶点数组对象：Vertex Array Object，VAO
+    //顶点缓冲对象：Vertex Buffer Object，VBO
+    unsigned int VBO,VAO;
+    glGenVertexArrays(1,&VAO);
+    glGenBuffers(1,&VBO);
+    glBindVertexArray(VAO);//
+    glBindBuffer(GL_ARRAY_BUFFER,VBO);
+    glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
+    glBindVertexArray(0);
+    //循环渲染
+    while(!glfwWindowShouldClose(window)){
+        processInput(window);
+        glClearColor(0.3f,0.5f,0.8f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shaderProgram);//使用这个着色器程序
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES,0,6);//绘制三角形，六个顶点
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    //关闭的时候要删掉
+    glDeleteVertexArrays(1,&VAO);
+    glDeleteBuffers(1,&VBO);
+    glfwTerminate();//终止
+    return 0;
+}
+void processInput(GLFWwindow *window){
+    if(glfwGetKey(window,GLFW_KEY_ESCAPE) == GLFW_PRESS){
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void framebuffer_size_callback(GLFWwindow *window,int width,int height){
+    glViewport(0,0,width,height);
+}
+
