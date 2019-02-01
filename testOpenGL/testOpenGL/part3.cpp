e48ac29b241ce663f139e289237d86a8b77ccbae
delevinying <delevinyyc@gmail.com>
2019-02-01 16:48:53 +0800

    3.着色器
    着色器

--- testOpenGL/testOpenGL/part3.cpp
+++ testOpenGL/testOpenGL/part3.cpp
@@ -25,9 +25,10 @@ const char *vertexShaderSoucre = "#version 330 core\n"
 "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"   FragColor = ourColor;\n"
 "}\n\0";
 
 
@@ -113,14 +114,12 @@ int main(){
         processInput(window);
         glClearColor(0.3f,0.5f,0.8f,1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
         glUseProgram(shaderProgram);//使用这个着色器程序
-        
         float timeValue = glfwGetTime();//
         float greenValue = sin(timeValue)/2.0f + 0.9f;
         int vertexColorLocation = glGetUniformLocation(shaderProgram,"ourColor");
         glUniform4f(vertexColorLocation,0.0f,greenValue,0.0f,1.0f);
-//        glBindVertexArray(VAO);
+        //glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES,0,3);//绘制三角形
         glfwSwapBuffers(window);
         glfwPollEvents();
